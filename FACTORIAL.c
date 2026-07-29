92c7a1ec951d9e65fde1e04f78114f7afe02a94d
SAKET-SUMAN <notsaket8@gmail.com>
2026-07-29 16:32:39 +0530

    Refactor FACTORIAL.c for improved readability

--- FACTORIAL.c
+++ FACTORIAL.c
@@ -7,21 +7,16 @@ int main(){
     int a;
     printf("ENTER YOUR INTEGRAL NUMBER FOR IT`S FACTORIAL :- ");
     scanf("%d",&a);
-
+    
     if(a<0){
         printf("INVALID INPUT :(");
     }else{
         fact(a);}
-
     }
     
-    
-
-
 int fact(int c){
     int a=1,b=1;
     for(a=1;a<=c;a++){
         b=a*b;         
  }printf("%d! = %d",c,b);
-
 }
